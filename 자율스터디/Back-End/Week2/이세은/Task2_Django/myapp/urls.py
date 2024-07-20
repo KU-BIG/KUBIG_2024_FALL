826ecbae77a284543921de9a943dd0ef92247fa3
이세은 <139235414+seeedata@users.noreply.github.com>
2024-07-20 23:47:38 +0900

    Add files via upload

---
+++ 자율스터디/Back-End/Week2/이세은/Task2_Django/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('', views.index),
+    path('create/', views.create),
+    path('read/<id>/', views.read),
+    path('update/<id>/', views.update),
+    path('delete/', views.delete)
+]
